178d2157b3e647b88ae7364453a8afe7c8f44ec0
ashini reshana nanayakkara <132426734+NanayakkaraAR@users.noreply.github.com>
2025-05-08 21:46:45 +0530

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.scss';
+import 'swiper/css'; 
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+
+import Routes from './config/Routes';
+
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Route render={props => (
+                <>
+                    <Header {...props}/>
+                    <Routes/>
+                    <Footer/>
+                </>
+            )}/>
+    </BrowserRouter>
   );
 }
 
